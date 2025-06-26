9735938fc72b00b81bf566b5bb59f2a8ef996025
PallaviSingh26 <113381278+PallaviSingh26@users.noreply.github.com>
2025-06-26 09:44:31 -0700

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,10 @@
+{
+  "version": 2,
+  "name": "my-static-site",
+  "builds": [
+    { "src": "index.html", "use": "@vercel/static" }
+  ],
+  "routes": [
+    { "src": "/(.*)", "dest": "/index.html" }
+  ]
+}
